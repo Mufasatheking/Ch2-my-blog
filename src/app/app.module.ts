f91b59ec0f70ec32c614d8cd3ede109a3d69e72c
Mustafa Aleem <maleem@intralinks.com>
2023-03-04 23:59:55 +0000

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { ContactModule } from './contact/contact.module';
+import { CoreModule } from './core/core.module';
+import { NgModule } from '@angular/core';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ContactModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class CoreModule { }
